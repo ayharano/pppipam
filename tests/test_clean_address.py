ec5f4e6e7c14785bbb7680aa2eed06fb0be8b19b
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 13:17:38 -0300

    Added new test IPv4 address value and a new error.

--- tests/test_clean_address.py
+++ tests/test_clean_address.py
@@ -18,3 +18,7 @@ class clean_address_TestCase(unittest.TestCase):
             clean_address("192.0.2.1"),
             ipaddress.IPv4Address("192.0.2.1"),
         )
+        self.assertEqual(
+            clean_address("203.0.113.128"),
+            ipaddress.IPv4Address("203.0.113.128"),
+        )
