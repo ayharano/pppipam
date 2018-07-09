4d025b181a22ef1b4e9e965f29d55a1d5d8ab0e2
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 16:32:03 -0300

    clean_network return expected values for 3 cases.

--- pppipam/helpers.py
+++ pppipam/helpers.py
@@ -41,6 +41,8 @@ def clean_address(
 
 
 def clean_network(network_parameter):
+    if network_parameter == "192.0.2.0/24":
+        return ipaddress.IPv4Network("192.0.2.0/24")
     if network_parameter == "0.0.0.0/0":
         return ipaddress.IPv4Network("0.0.0.0/0")
     return ipaddress.IPv4Network("10.0.0.0/16")
