20c43ed44d928bf90c7d997fbcb848d43a9efb70
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 15:43:44 -0300

    Added documantation of clean_address.

--- pppipam/helpers.py
+++ pppipam/helpers.py
@@ -7,6 +7,18 @@ import ipaddress
 
 
 def clean_address(address_parameter):
+    """Process given parameter as an Address instance.
+
+    If parameter results into a valid IPv4 or IPv6 address,
+    it respectively returns IPv4Address or IPv6Address.
+    Otherwise, returns None.
+
+    Args:
+        address_parameter: value to be processed as an IP address.
+
+    Returns:
+        IPv4Address instance, IPv6Address instance or None.
+    """
     value = None
     try:
         value = ipaddress.ip_address(address_parameter)
