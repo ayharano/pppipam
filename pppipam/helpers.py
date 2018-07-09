b80386743f30b3398efd628efbad437674076486
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 13:26:25 -0300

    Refactored clean_address.
    - Returns IPv4Address from address_parameter.

--- pppipam/helpers.py
+++ pppipam/helpers.py
@@ -7,8 +7,9 @@ import ipaddress
 
 
 def clean_address(address_parameter):
-    if address_parameter == "198.51.100.255":
-        return ipaddress.IPv4Address("198.51.100.255")
-    if address_parameter == "203.0.113.128":
-        return ipaddress.IPv4Address("203.0.113.128")
-    return ipaddress.IPv4Address("192.0.2.1")
+    return ipaddress.IPv4Address(address_parameter)
+    # if address_parameter == "198.51.100.255":
+    #     return ipaddress.IPv4Address("198.51.100.255")
+    # if address_parameter == "203.0.113.128":
+    #     return ipaddress.IPv4Address("203.0.113.128")
+    # return ipaddress.IPv4Address("192.0.2.1")
