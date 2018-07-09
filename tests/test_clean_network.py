7b34dcfc4e5cb8355a2dd69dff1bc66c10cb07c4
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 16:15:29 -0300

    Added test for valid IPv4 network and a new error.

--- tests/test_clean_network.py
+++ tests/test_clean_network.py
@@ -3,4 +3,17 @@
 
 """Tests for clean_network function of `pppipam` package."""
 
+import unittest
+
 from pppipam.helpers import clean_network
+
+
+class clean_network_TestCase(unittest.TestCase):
+    """Tests for clean_network."""
+
+    def test_clean_network_valid_ipv4_network_as_str(self):
+        """Test for valid IPv4 Network correctly instantiated from str."""
+        self.assertEqual(
+            clean_network("10.0.0.0/16"),
+            ipaddress.IPv4Network("10.0.0.0/16"),
+        )
