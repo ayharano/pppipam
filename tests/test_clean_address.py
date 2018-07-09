ce92bdc530dad7e0b07d25b78741fa9f44843dd5
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 13:02:22 -0300

    Added test for valid IPv4 address and a new error.

--- tests/test_clean_address.py
+++ tests/test_clean_address.py
@@ -3,5 +3,18 @@
 
 """Tests for clean_address function of `pppipam` package."""
 
+import ipaddress
+import unittest
 
 from pppipam.helpers import clean_address
+
+
+class clean_address_TestCase(unittest.TestCase):
+    """Tests for clean_address."""
+
+    def test_clean_address_valid_ipv4_address(self):
+        """Test for valid IPv4 Addresses correctly instantiated."""
+        self.assertEqual(
+            clean_address("192.0.2.1"),
+            ipaddress.IPv4Addres("192.0.2.1"),
+        )
