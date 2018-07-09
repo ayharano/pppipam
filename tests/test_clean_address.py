560519cc74aa9782a148ae56c580f458a68ee578
Alexandre Harano <alexandre@harano.net.br>
2018-07-09 13:11:35 -0300

    Adjusted test error and a new AssertionError.
    - Fixed error in test.
    - Restricted test scope to str parameters.
    - clean_address does not return expected result.

--- tests/test_clean_address.py
+++ tests/test_clean_address.py
@@ -12,9 +12,9 @@ from pppipam.helpers import clean_address
 class clean_address_TestCase(unittest.TestCase):
     """Tests for clean_address."""
 
-    def test_clean_address_valid_ipv4_address(self):
-        """Test for valid IPv4 Addresses correctly instantiated."""
+    def test_clean_address_valid_ipv4_address_as_str(self):
+        """Test for valid IPv4 Addresses correctly instantiated from str."""
         self.assertEqual(
             clean_address("192.0.2.1"),
-            ipaddress.IPv4Addres("192.0.2.1"),
+            ipaddress.IPv4Address("192.0.2.1"),
         )
