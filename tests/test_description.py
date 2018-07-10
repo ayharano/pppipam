e850aa6a651e8b8a69bf7cb461907d4317f28f34
Alexandre Harano <alexandre@harano.net.br>
2018-07-10 00:37:56 -0300

    Added test with setUp using AddressSpace, error.

--- tests/test_description.py
+++ tests/test_description.py
@@ -3,4 +3,17 @@
 
 """Tests for description methods of pppipam.AddressSpace instances."""
 
+import unittest
 from pppipam.pppipam import AddressSpace
+
+
+class AddressSpace_description_TestCase(unittest.TestCase):
+    """Tests for description related methods."""
+
+    def setUp(self):
+        """Set up test fixtures with new AddressSpace."""
+        self.address_space = AddressSpace()
+
+    def test_describe_address(self):
+        """Add valid IP address with non-empty str description."""
+        pass
