416fc25c7d36eaa1c8d05d9a8c6b9b05e7c61e7d
Alexandre Harano <alexandre@harano.net.br>
2018-07-10 00:43:56 -0300

    Added an AddressSpace instance test, error.

--- tests/test_description.py
+++ tests/test_description.py
@@ -14,6 +14,10 @@ class AddressSpace_description_TestCase(unittest.TestCase):
         """Set up test fixtures with new AddressSpace."""
         self.address_space = AddressSpace()
 
+    def test_address_space_as_AddressSpace(self):
+        """Validate if address_space is instance of AddressSpace."""
+        self.assertIsInstance(self.address_space, AddressSpace)
+
     def test_describe_address(self):
         """Add valid IP address with non-empty str description."""
         pass
