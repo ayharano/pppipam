492616c6db263703fc680c783b3a4b75155291d6
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 09:05:35 -0300

    Used false evaluation to pass, error in old test.
    - This modification introduced new error in old test for evaluating description as False before checking if it is an empty str.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -5,6 +5,8 @@
 
 class AddressSpace:
     def describe(self, *, ip_parameter, description):
+        if not description:
+            raise TypeError("description must be str")
         if description == "":
             raise ValueError("No empty description allowed")
         return True
