edd9740eee9e2e546132a6d1af966eb6a8d923dc
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 09:17:24 -0300

    Removing old alidation still passes, no error.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -9,10 +9,4 @@ class AddressSpace:
             raise ValueError("No empty description allowed")
         if not isinstance(description, str):
             raise TypeError("description must be str")
-        if isinstance(description, set):
-            raise TypeError("description must be str")
-        if description == 123:
-            raise TypeError("description must be str")
-        if not description:
-            raise TypeError("description must be str")
         return True
