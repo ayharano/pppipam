75298819d6b52c37864f88fa8964a36872205715
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 09:15:19 -0300

    Set instance verification in describe, no error.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -7,6 +7,8 @@ class AddressSpace:
     def describe(self, *, ip_parameter, description):
         if description == "":
             raise ValueError("No empty description allowed")
+        if isinstance(description, set):
+            raise TypeError("description must be str")
         if description == 123:
             raise TypeError("description must be str")
         if not description:
