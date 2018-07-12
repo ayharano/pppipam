2c147fc3bb4a0fd6d7bf1eddf42e32d91fd88da7
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 23:40:07 -0300

    Added empty lines to improve readability, no error

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -7,6 +7,7 @@ import pppipam.helpers as helpers
 
 
 class AddressSpace:
+
     def describe(self, *, ip_parameter, description):
         if isinstance(ip_parameter, int):
             raise TypeError("ip_parameter must not be int")
@@ -14,8 +15,10 @@ class AddressSpace:
         as_network = helpers.clean_network(ip_parameter)
         if not as_address and not as_network:
             raise TypeError("ip_parameter must be a valid IP parameter")
+
         if description == "":
             raise ValueError("No empty description allowed")
         if not isinstance(description, str):
             raise TypeError("description must be str")
+
         return True
