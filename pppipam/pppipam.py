bb7cef893b70f17364c8d11bc1e6b12c42dd3f10
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 23:38:04 -0300

    Removed commented out code, no error.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -14,12 +14,6 @@ class AddressSpace:
         as_network = helpers.clean_network(ip_parameter)
         if not as_address and not as_network:
             raise TypeError("ip_parameter must be a valid IP parameter")
-        # if isinstance(ip_parameter, set):
-        #     raise TypeError("ip_parameter must be a valid IP parameter")
-        # if ip_parameter == 123:
-        #     raise TypeError("ip_parameter must be a valid IP parameter")
-        # if not ip_parameter:
-        #     raise TypeError("ip_parameter must be a valid IP parameter")
         if description == "":
             raise ValueError("No empty description allowed")
         if not isinstance(description, str):
