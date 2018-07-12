36cfb4c90c4e7945460a6260ef6eea6ebbad88d2
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 22:53:48 -0300

    False evaluation in IP parameter test, no error.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -5,6 +5,8 @@
 
 class AddressSpace:
     def describe(self, *, ip_parameter, description):
+        if not ip_parameter:
+            raise TypeError("ip_parameter must be a valid IP parameter")
         if description == "":
             raise ValueError("No empty description allowed")
         if not isinstance(description, str):
