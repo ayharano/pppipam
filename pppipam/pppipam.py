e3973a42eba354eaebb77a464054da174b163e3a
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 23:35:09 -0300

    If int IP parameter, raise error; no error.
    - Instead of changing clean_address/clean_network, restricted parameter in describe to let clean_address/clean_network still be like original functions with exception handling as None.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -8,6 +8,8 @@ import pppipam.helpers as helpers
 
 class AddressSpace:
     def describe(self, *, ip_parameter, description):
+        if isinstance(ip_parameter, int):
+            raise TypeError("ip_parameter must not be int")
         as_address = helpers.clean_address(ip_parameter)
         as_network = helpers.clean_network(ip_parameter)
         if not as_address and not as_network:
