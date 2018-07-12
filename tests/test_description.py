93000a63c3a14d7e51cebce1f7b38ff7dbae503c
Alexandre Harano <alexandre@harano.net.br>
2018-07-11 22:55:52 -0300

    Non-zero int to IP parameter test, fail.

--- tests/test_description.py
+++ tests/test_description.py
@@ -97,6 +97,18 @@ class AddressSpace_description_TestCase(unittest.TestCase):
             invalid_ip_parameter,
             "Invalid IP parameter should raise TypeError"
         )
+        invalid_ip_parameter = False
+        try:
+            self.address_space.describe(
+                description="parameter as non-zero int",
+                ip_parameter=123,
+            )
+        except TypeError:
+            invalid_ip_parameter = True
+        self.assertTrue(
+            invalid_ip_parameter,
+            "Invalid IP parameter should raise TypeError"
+        )
 
     def test_describe_description_not_str_typeerror(self):
         """Non-str description should raise TypeError."""
