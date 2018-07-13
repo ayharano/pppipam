6b8d512e6dd9dfea72c75761f8a86b9014c9f9a9
Alexandre Harano <alexandre@harano.net.br>
2018-07-12 22:44:17 -0300

    Parameter-less method raises ValueError, pass.

--- pppipam/pppipam.py
+++ pppipam/pppipam.py
@@ -138,7 +138,7 @@ class AddressSpace:
         return described
 
     def describe_new_delegated_network(self):
-        pass
+        raise TypeError()
 
     def description(self, ip_parameter: IPParameter) -> typing.Optional[str]:
         """Retrieve a description of an IP address or IP network.
