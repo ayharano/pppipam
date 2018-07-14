aa3d33c1dd3a2bedc75a2384911b291c9a1b44b7
Alexandre Harano <harano@nic.br>
2018-07-14 00:28:35 -0300

    Added more test values, pass.

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -87,11 +87,24 @@ class AddressSpace_delete_TestCase(unittest.TestCase):
                     "Deleting a described IP object should return True.",
                 )
 
-
     def test_delete_after_deleting_object_description_should_be_none(self):
         """Deleting a described IP object should return True."""
         for parameter_description in (
             ("203.0.113.128", "an IPv4 test net address"),
+            ("2001:db8:abcd::/48", "an IPv6 doc subnet"),
+            ("123.123.123.123", "123 is nice"),
+            ("203.0.113.129", "Second subnet gateway"),
+            ("192.0.2.123", "123 is nice, test net"),
+            ("2001:db8::abcd:ef12", "ipv6 doc address"),
+            ("2000::", "first global address for now"),
+            ("fe80::", "even link-local?"),
+            ("10.0.0.0/16", "a private IPv4 network"),
+            ("203.0.113.128/25", "part of a test net"),
+            ("0.0.0.0/0", "whole IPv4 address space as network"),
+            ("2001:db8:1234:5678::/64", "a documentation IPv6 network"),
+            ("fe80::/64", "IPv6 link-local network"),
+            ("fd01:2345:6789::/48", "a ~random~ IPv6 unique-local network"),
+            ("::/0", "whole IPv6 address space as network"),
         ):
             with self.subTest(parameter_description=parameter_description):
                 self.assertTrue(
