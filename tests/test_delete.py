4101ac53252f1b3cb2a6c331bf599d29efee292b
Alexandre Harano <harano@nic.br>
2018-07-13 23:23:56 -0300

    Added no positional args test, pass.
    - delete explicitly raises TypeError.

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -25,3 +25,15 @@ class AddressSpace_delete_TestCase(unittest.TestCase):
             no_empty_arguments,
             "Empty arguments should not be accepted in delete",
         )
+
+    def test_delete_keyword_only_no_positional_arguments(self):
+        """delete method as keyword only, without positional parameters."""
+        no_positional_arguments = False
+        try:
+            self.address_space.delete("203.0.113.128", True)
+        except TypeError:
+            no_positional_arguments = True
+        self.assertTrue(
+            no_positional_arguments,
+            "No positional argument should be accepted in delete",
+        )
